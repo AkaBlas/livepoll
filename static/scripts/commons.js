2c1bf994005da38ba243c3ac33c34daa8e65f0d7
Hinrich Mahler <22366557+Bibo-Joshi@users.noreply.github.com>
2023-08-27 11:25:37 +0200

    Make Websockets auto-reconnect

--- static/scripts/commons.js
+++ static/scripts/commons.js
@@ -1,19 +1,31 @@
 const baseUrl = window.location.protocol + "//" + window.location.host;
 const apiUrl = baseUrl + "/api";
 
+function randomIntFromInterval(min, max) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 function build_websocket(endpoint) {
-  const WSocket = new WebSocket(
+  const WSocket = new ReconnectingWebSocket(
     "ws://" + window.location.host + "/" + endpoint,
+    null,
+    {
+      debug: true,
+      reconnectInterval: randomIntFromInterval(500, 1500),
+      maxReconnectInterval: 5000,
+      reconnectDecay: 1.1,
+    },
   );
 
-  WSocket.onclose = function (event) {
-    console.log(event.toString());
-    alert("Lost connection to server. Please reload the page.");
-  };
-  WSocket.onerror = function (event) {
-    console.log(event.toString());
-    alert("Error in connection to server. Please reload the page.");
-  };
+  // WSocket.onclose = function (event) {
+  //   console.log(event.toString());
+  //   console.log("Lost connection to server. Please reload the page.");
+  // };
+  // WSocket.onerror = function (event) {
+  //   console.log(event.toString());
+  //   console.log("Error in connection to server. Please reload the page.");
+  // };
 
   return WSocket;
 }
